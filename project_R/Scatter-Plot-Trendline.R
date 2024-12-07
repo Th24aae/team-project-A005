41020b8a6f67b652dc6e1a368e2f8f1f3f4bde74
Th24aae <Th24aae@herts.ac.uk>
2024-12-07 20:42:27 +0000

    Scatter plot Trendline

---
+++ project_R/Scatter-Plot-Trendline.R
@@ -0,0 +1,10 @@
+library(ggplot2)
+ggplot(Earthquakes_v3, aes(x = DEPTH, y = MAGNITUDE)) +
+  geom_point(color = "pink") +  # Scatter points
+  geom_smooth(method = "lm", color = "black", se = FALSE) +  # Adding a linear regression line
+  labs(title = "Scatter Plot of Depth vs Magnitude with a Line",
+       x = "Depth (km)",
+       y = "Magnitude") +
+  theme_minimal()
+
+
